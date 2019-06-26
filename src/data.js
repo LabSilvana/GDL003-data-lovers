9ef5d3eab7958bb5d936050e886049f27c7ca155
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-06-26 14:26:49 -0500

    agregue boton de personajes

--- src/data.js
+++ src/data.js
@@ -1,37 +1,7 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const data = () => {
-  return 'rickandmorty.js';
-};
 
-const sortBY = () => {
-  return '';
-};
-
-const filtrado = () => {
-  return 'filtrado';
-};
-
-const mostrar = () => {
-  return 'mostrar';
-
-};
-
-document.querySelector('#botonesPersonajes').addEventListener('click', traerDatos());
-function traerDatos(){
-const xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'rickandmorty.json', true);
-xhttp.send();
-xhttp.onreadystatechange = function(){
-
-  if(this.readyState == 4 && this.status == 200){
-
-    console.log(this.responseText);
-  } 
-}
-
-}
 
 window.example = {
   example,
