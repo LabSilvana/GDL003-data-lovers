b73f43c157ec9f86fe585e2cc2acbaf15ba00ca2
Silvana <silvana15_91@hotmail.com>
2019-07-02 14:14:10 -0500

    Se agrego status

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { data } from './data.js';
-let info_html = document.getElementById("character_list")
+let info_html = document.getElementById("character_list");
 let info = data();
 //console.log(info);
 //let character = info[0];
@@ -8,6 +8,8 @@ info.forEach(function(character){
     //console.log(character.name);
 
     info_html.innerHTML += `<p>${character.name}</p>`;
+    info_html.innerHTML += `<p>${character.status}</p>`;
+    
 
 
 
