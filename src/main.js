82e6bb62b10a4f33ba710904e1efc54e5768c5a4
Silvana <silvana15_91@hotmail.com>
2019-07-03 18:10:10 -0500

    Se termino de filtrar los datos

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ let info = data();
 //console.log(character.name);
 
 let button= document.getElementById("Femenino");
+let button2= document.getElementById("Masculino");
+let button3= document.getElementById("Indefinido")
 let button_character = document.getElementById("boton_personajes");
 
 button_character.addEventListener('click', () => {
@@ -18,6 +20,28 @@ button_character.addEventListener('click', () => {
 
 button.addEventListener('click',() => {
     let result = filterData(info,"Female");
+    //console.log(result);
+    info_html.innerHTML = "";
+    result.forEach(function(character) {
+        console.log(character.name);
+        print_character(character);
+    }); 
+
+});
+
+button2.addEventListener('click',() => {
+    let result = filterData(info,"Male");
+    //console.log(result);
+    info_html.innerHTML = "";
+    result.forEach(function(character) {
+        console.log(character.name);
+        print_character(character);
+    }); 
+
+});
+
+button3.addEventListener('click',() => {
+    let result = filterData(info,"unknown");
     //console.log(result);
     info_html.innerHTML = "";
     result.forEach(function(character) {
