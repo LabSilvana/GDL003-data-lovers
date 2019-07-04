e346e6c719a7a67050d0bb062a3f3941d2c3c4e7
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-07-04 18:40:22 -0500

    diseño de las fichas

--- src/main.js
+++ src/main.js
@@ -12,56 +12,56 @@ let button3= document.getElementById("Indefinido");
 let button_character = document.getElementById("boton_personajes");
 
 button_character.addEventListener('click', () => {
-    console.log("evento");
-    info.forEach(function(character) {
-        console.log(character.name);
-      
-    }); 
+   console.log("evento");
+   info.forEach(function(character) {
+       console.log(character.name);
+
+   });
 });
 
 button.addEventListener('click',() => {
-    let result = filterData(info,"Female");
-    info_html.innerHTML = "";
-    result.forEach(function(character) {
-        console.log(character.name);
-        info_html.innerHTML += print_character(character);
-    }); 
+   let result = filterData(info,"Female");
+   info_html.innerHTML = "";
+   result.forEach(function(character) {
+       console.log(character.name);
+       info_html.innerHTML += print_character(character);
+   });
 
 });
 
 button2.addEventListener('click',() => {
-    let result = filterData(info,"Male");
-    info_html.innerHTML = "";
-    result.forEach(function(character) {
-        console.log(character.name);
-        info_html.innerHTML += print_character(character);
-    }); 
+   let result = filterData(info,"Male");
+   info_html.innerHTML = "";
+   result.forEach(function(character) {
+       console.log(character.name);
+       info_html.innerHTML += print_character(character);
+   });
 
 });
 
 button3.addEventListener('click',() => {
-    let result = filterData(info,"unknown");
-    info_html.innerHTML = "";
-    result.forEach(function(character) {
-        console.log(character.name);
-        info_html.innerHTML += print_character(character);
-    }); 
+   let result = filterData(info,"unknown");
+   info_html.innerHTML = "";
+   result.forEach(function(character) {
+       console.log(character.name);
+       info_html.innerHTML += print_character(character);
+   });
 
 });
 
 
 function print_character(character) {
 
-    return '<div class="card">' +
-                `<img src="${character.image}">` +
-                '<div class="container">' +
-                    `<h4>${character.name}</h4>` +
-                    `<p>${character.status}</p>` +
-                    `<p>${character.species}</p>` +
-                    `<p>${character.gender}</p>` +
-                    `<p>${character.origin.name}</p>` +
-                    `<p>${character.location.name}</p>` +
-                '</div>' +
-            '</div>';
-    
+   return '<div class="card">' +
+               `<img src="${character.image}">` +
+               '<div class="container">' +
+                   `<h4>${character.name}</h4>` +
+                   `<p>${character.status}</p>` +
+                   `<p>${character.species}</p>` +
+                   `<p>${character.gender}</p>` +
+                   `<p>${character.origin.name}</p>` +
+                   `<p>${character.location.name}</p>` +
+               '</div>' +
+           '</div>';
+
 }
